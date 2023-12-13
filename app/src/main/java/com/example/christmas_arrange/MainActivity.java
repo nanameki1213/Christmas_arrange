1c8d303415ece8c24a4dc295b629668b7ee42b44
Sonoda-Arito <nekotoinu2525@gmail.com>
2023-12-13 22:45:48 +0900

    ダイアログ追加版

--- app/src/main/java/com/example/christmas_arrange/MainActivity.java
+++ app/src/main/java/com/example/christmas_arrange/MainActivity.java
@@ -1,21 +1,23 @@
 package com.example.christmas_arrange;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_FOLDER_REQUEST_CODE = 2;
 
     Button btnSave;
     Button btnPlay;
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+    protected void onCreate(Bundle instanceState1) {
+        super.onCreate(instanceState1);
         setContentView(R.layout.activity_main);
 
         btnSave = findViewById(R.id.btnSave);
@@ -56,12 +58,5 @@ public class MainActivity extends AppCompatActivity {
     public void onPlayButtonClick(View view) {
 
     }
-
-
-
-
-
-
-
 
 }
