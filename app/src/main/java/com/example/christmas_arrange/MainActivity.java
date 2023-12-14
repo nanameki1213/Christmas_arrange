51352e40acfc99ad849ccbac5fc85bd4a56125a9
Sonoda-Arito <nekotoinu2525@gmail.com>
2023-12-14 16:28:43 +0900

    ダイアログ追加版

--- app/src/main/java/com/example/christmas_arrange/MainActivity.java
+++ app/src/main/java/com/example/christmas_arrange/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.christmas_arrange;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -47,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 .setMessage("保存先のパスを設定してください")
                 .setPositiveButton("OK", (dialog, which) -> {
                     // OKボタン押下時に実行したい処理を記述
+                    openFolderChooser();;
                 })
                 .setNegativeButton("キャンセル", (dialog, which) -> {
                     // キャンセルボタン押下時に実行したい処理を記述
@@ -57,6 +63,42 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onPlayButtonClick(View view) {
 
+    }
+
+    private void openFolderChooser() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        startActivityForResult(intent, PICK_FOLDER_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_FOLDER_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            Uri treeUri = data.getData();
+            if (treeUri != null) {
+                String selectedFolderPath = getDocumentPath(treeUri);
+                handleSelectedFolder(selectedFolderPath);
+            }
+        }
+    }
+
+    private String getDocumentPath(Uri treeUri) {
+        String documentId = DocumentsContract.getTreeDocumentId(treeUri);
+        String[] split = documentId.split(":");
+        if (split.length >= 2) {
+            String type = split[0];
+            if ("primary".equalsIgnoreCase(type)) {
+                return "/" + split[1];
+            }
+        }
+        return null;
+    }
+
+    private void handleSelectedFolder(String folderPath) {
+        // 選択されたフォルダのパスを使用して処理を行う
+        // 例: パスをログに表示
+        System.out.println("Selected Folder Path: " + folderPath);
     }
 
 }
